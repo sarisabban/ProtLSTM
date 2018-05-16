4ec3cc73e97740852f2745573d6698c748b48ad9
acresearch <sari.sabban@gmail.com>
2018-05-16 13:36:28 +0300

    general upload

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # ProtLSTM
 An LSTM neural network to generate novel protein sequences
+
+## Requirements:
+1. Use the following command (in GNU/Linux) to install all necessary programs and python modules for this script to run successfully:
+
+`sudo apt update && sudo apt full-upgrade && sudo apt install python3-pip python3-pandas python3-numpy python3-tensorflow && pip3 install keras`
+
+## Description:
+This is a script that uses Deep Learning Neural Networks, specifically an LSTM neural network, to 
+
+## How To Use:
+1. The database SS.cvs and SEQ.csv are already provided, but they can be generated using the Database.py script from the [AIDenovo project](https://github.com/sarisabban/AIDeNovo).
+2. A trained model is also provided as model.h5 so you do not have to run the training everytime
+
+3. To generate a protein's secondary structure use the following command:
+
+`python3 lstm.py `
+
+The result would be a string that looks like this:
+
+``
+
+4. To generate a protein sequence use the following command:
+
+`python3 lstm.py `
+
+The result should look like this:
+
+``
+
+But also useful information would be the BLAST and the PSIPRED results, where the BLAST shows how similar the generated sequence is to all knows protein sequences, while the PSIPRED results show its predicted secodary structure.
+
+BLAST result:
+
+``
+
+PSIPRED result:
+
+``
---
+++ lstm.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+import numpy
+import keras
+
+#Import text
+text = open('SS_raw.txt' , 'r').read()
+chars = sorted(list(set(text)))
+chars = ['-' if x == '\n' else x for x in chars]
+#chars.remove('\n')
+chars_indices = dict((c , i) for i , c in enumerate(chars))
+indices_chars = dict((i , c) for i , c in enumerate(chars))
+maxlen = 150
+step = 1
+#Generate sentences and next charachters
+text2 = open('SS_raw.txt' , 'r')
+sentences = []
+next_chars = []
+for line in text2:
+	line = line.strip()
+	#Generate sentences
+	sentlen = maxlen-1
+	for i in range(0 , len(line)-1 , step):
+		sentence = (line[0 : i+1] + sentlen * '-')
+		sentences.append(sentence)
+		sentlen -= 1
+		#Generate next charachter
+		next_char = line[i+1]
+		next_chars.append(next_char)
+#Vectorise - (sentances , sentance length , charachters)
+X = numpy.zeros((len(sentences) , maxlen , len(chars)) , dtype = numpy.bool)
+Y = numpy.zeros((len(sentences) , len(chars)) , dtype = numpy.bool)
+#One-hot encoding
+for i , sentence in enumerate(sentences):
+	for t , char in enumerate(sentence):
+		X[i , t , chars_indices[char]] = 1
+Y[i , chars_indices[next_chars[i]]] = 1
+
+#TensorBoard log (tensorboard --logdir=./logs)
+tensorboard = keras.callbacks.TensorBoard(log_dir = './logs')
+
+#Setup neural network
+model = keras.models.Sequential()
+model.add(keras.layers.LSTM(3 , input_shape = (maxlen , len(chars)) , activation = 'relu'))
+#model.add(keras.layers.LSTM(3 , dropout = 0.25 , activation = 'relu'))
+model.add(keras.layers.Dense(len(chars) , activation = 'softmax'))
+
+#Compile model
+model.compile(	keras.optimizers.Adam(lr = 0.01) , 
+		loss = 'categorical_crossentropy' , 
+		metrics = ['accuracy'])
+model.summary()
+
+#Train model
+model.fit(	X , Y , 
+		batch_size = 60 , 
+		epochs = 10 , 
+		verbose = 1 , 
+		callbacks = [tensorboard])
+
+#Save Model
+#model.save('model.h5')
+
+#Load Model
+#model.load_weights('model.h5')
+
+'''
+#Generate
+print('--------------------')
+start_index = random.randint(0 , len(text) - maxlen - 1)
+sentence = text[start_index : start_index + maxlen]
+print(sentence)
+for iter in range(100):
+	#One-hot encode that sentance
+	x_pred = numpy.zeros((1 , maxlen , len(chars)))
+	for t , char in enumerate(sentence):
+		x_pred[0 , t , chars_indices[char]] = 1.0
+	#Use that tensor to make a prediction of the next charachter
+	preds = model.predict(x_pred , verbose = 0)[0]
+	#Decode that charachter
+	temperature = 0.2
+	preds = numpy.asarray(preds).astype('float64')
+	preds = numpy.log(preds) / temperature
+	exp_preds = numpy.exp(preds)
+	preds = exp_preds / numpy.sum(exp_preds)
+	probas = numpy.random.multinomial(1 , preds , 1)
+	next_index = numpy.argmax(probas)
+	next_char = indices_chars[next_index]
+	sentence = sentence[1 : ] + next_char
+	sys.stdout.write(next_char)
+	sys.stdout.flush()
+print('\n--------------------')
+'''
