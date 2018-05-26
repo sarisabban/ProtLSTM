d1791f85f52c27d64f7d6be59ff23fdd33ad94f1
Sari Sabban <sari.sabban@gmail.com>
2018-05-26 21:09:55 +0300

    Update protlstm.py

--- protlstm.py
+++ protlstm.py
@@ -58,10 +58,10 @@ def SS(calc):
 
 	if calc == 'train':
 		#TensorBoard log (tensorboard --logdir=./logs)
-		tensorboard = keras.callbacks.TensorBoard(log_dir = './' , write_grads = True)
+		tensorboard = keras.callbacks.TensorBoard(log_dir = './' , histogram_freq = 1 , write_grads = True)
 		#Train model
 		model.summary()
-		model.fit(X , Y , batch_size = 128 , epochs = 120 , validation_split = 0.2 , verbose = 2 , callbacks = [tensorboard])
+		model.fit(X , Y , batch_size = 512 , epochs = 120 , validation_split = 0.2 , verbose = 2 , callbacks = [tensorboard])
 		#Save Model
 		model.save('SS.h5')
 
